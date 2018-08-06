eecc77e3d9df4ade476cf3218934264bb844924f
paulyao <yao.1981.cn@gmail.com>
2018-08-06 14:52:27 +0800

    Update dingding.py

--- pyspider/libs/dingding.py
+++ pyspider/libs/dingding.py
@@ -10,7 +10,7 @@ class DingTalk:
         textmod={"msgtype": "text","text": {"content":nowTime+' | '+ content},"at": {"atMobiles": [],"isAtAll":"false"}}
         textmod = json.dumps(textmod)
         header_dict = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',"Content-Type": "application/json"}
-        url='https://oapi.dingtalk.com/robot/send?access_token=39b87e5df2111731e65f80842036deb6848dfdc9ed07e7c2ee8b9fb55f23c234'
+        url='https://oapi.dingtalk.com/robot/send?access_token=XXXXXXXXXXXXXXXXXXXXXX'
         req = urllib2.Request(url=url,data=textmod,headers=header_dict)
         res = urllib2.urlopen(req)
         res = res.read()
